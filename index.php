fc43a60ad99fe7d927e4ae342390b6b2e5cf3c92
Candace Oppeng <candaceoppeng.me@gmail.com>
2020-03-26 10:58:39 +0100

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -5,15 +5,22 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
+
+
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=netland", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$servername;dbname=netland", $username, $password);
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
+  
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
 ?>
+
+
+<table>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -25,7 +32,55 @@ try {
 </head>
 
 <body>
-    <h1>Uhhhhh</h1>
+
+
+
+    <h1>Welkom op het netland beheerderspaneel</h1>
+  <table style="width:200px">
+    <tr><th>Series</th>
+    <th>Rating</th>
+    <td style="text-align:center"></td></tr>
+<?php
+
+$series = $pdo->query('SELECT title,rating FROM series');
+echo "<h1>Series</h1>";
+
+while ($show = $series->fetch()){
+    
+    echo "<tr><td>";
+    echo $show['title'].'</td>';
+    echo "<td >". $show['rating']."</td>";
+    echo "</tr>";
+    }
+ 
+
+?>
+
+</table>
+
+
+
+<table style="width:200px">
+<tr><th>Movies</th>
+    <th>Duur</th>
+    <td style="text-align:center"></td></tr>
+
+        <?php
+
+
+            $movies = $pdo->query('SELECT title,duur FROM movies');
+            echo "<h1>Movies</h1>";
+            while ($show = $movies->fetch()){
+                echo "<tr>";
+                echo "<td>".$show['title'].'</td><td>'. $show['duur'];"</td>";
+                echo "</tr>";
+                }
+        ?>
+
+
+
+
+</table>
 </body>
 
 </html>
